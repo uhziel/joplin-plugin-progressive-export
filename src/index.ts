3e528fd99c2dcea667b84766b918c0ccee13861b
uhziel <uhziel@gmail.com>
2025-01-12 20:54:46 +0800

    实现一个最基本的导出 md 工具

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,53 @@
 import joplin from 'api';
+import { FileSystemItem } from 'api/types';
+
+const fs = require('fs-extra');
+import path = require('path');
+
+function destDir(context:any) {
+	return context.destPath;
+}
+
+function resourceDir(context:any) {
+	return context.destPath + '/assets';
+}
+
+enum ModelType {
+	Note = 1,
+	Folder = 2,
+	Resource = 4,
+	Tag = 5,
+	NoteTag = 6,
+}
 
 joplin.plugins.register({
 	onStart: async function() {
-		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
+
+		await joplin.interop.registerExportModule({
+			description: 'Obsidian Export Directory',
+			format: 'obsidian',
+			target: FileSystemItem.Directory,
+			isNoteArchive: false,
+
+			onInit: async (context:any) => {
+				await fs.mkdirp(destDir(context));
+				await fs.mkdirp(resourceDir(context));
+			},
+
+			onProcessItem: async (context:any, _itemType:number, item:any) => {
+				if (_itemType !== ModelType.Note) {
+					return
+				}
+				const filePath = destDir(context) + '/' + item.title + '.md';
+				await fs.writeFile(filePath, item.body, 'utf8');
+			},
+
+			onProcessResource: async (context:any, _resource:any, filePath:string) => {
+				const destPath = resourceDir(context) + '/' + path.basename(filePath);
+				await fs.copy(filePath, destPath);
+			},
+
+			onClose: async (_context:any) => {},
+		});
 	},
 });
